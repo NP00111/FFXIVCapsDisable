b1506cce4a6383fe00658611733f6348a46eb36a
Xtrod <Xtrod@XTRO-DESKTOP>
2024-09-14 20:07:00 -0700

    Unhook on closing form. General Cleanup

--- FFXIV Disable Caps/Options.Designer.cs
+++ FFXIV Disable Caps/Options.Designer.cs
@@ -32,18 +32,19 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Options));
             timer_chk_ffxiv_process = new System.Windows.Forms.Timer(components);
             chkAutoStart = new CheckBox();
+            TrayIcon = new NotifyIcon(components);
             SuspendLayout();
             // 
             // timer_chk_ffxiv_process
             // 
             timer_chk_ffxiv_process.Enabled = true;
             timer_chk_ffxiv_process.Interval = 10000;
-            timer_chk_ffxiv_process.Tick += timer1_Tick;
+            timer_chk_ffxiv_process.Tick += timert_check_process;
             // 
             // chkAutoStart
             // 
             chkAutoStart.AutoSize = true;
-            chkAutoStart.Location = new Point(19, 11);
+            chkAutoStart.Location = new Point(12, 28);
             chkAutoStart.Name = "chkAutoStart";
             chkAutoStart.Size = new Size(162, 19);
             chkAutoStart.TabIndex = 0;
@@ -51,16 +52,26 @@
             chkAutoStart.UseVisualStyleBackColor = true;
             chkAutoStart.Click += chkAutoStart_Click;
             // 
+            // TrayIcon
+            // 
+            TrayIcon.Icon = (Icon)resources.GetObject("TrayIcon.Icon");
+            TrayIcon.Text = "notifyIcon1";
+            TrayIcon.Visible = true;
+            TrayIcon.Click += TrayIcon_Click;
+            // 
             // Options
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(354, 74);
+            ClientSize = new Size(282, 74);
             Controls.Add(chkAutoStart);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
             Name = "Options";
             Text = "FFXIV CapsLock Disabler";
+            FormClosing += Options_FormClosing;
             Load += Options_Load;
+            Resize += Options_Resize;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -69,5 +80,6 @@
 
         private System.Windows.Forms.Timer timer_chk_ffxiv_process;
         private CheckBox chkAutoStart;
+        private NotifyIcon TrayIcon;
     }
 }
--- FFXIV Disable Caps/Options.cs
+++ FFXIV Disable Caps/Options.cs
@@ -1,18 +1,13 @@
 using Microsoft.Win32;
-using System;
-using System.ComponentModel;
 using System.Diagnostics;
-using System.Drawing;
-using System.Resources;
 using System.Runtime.InteropServices;
-using System.Windows.Forms;
-using static System.Windows.Forms.DataFormats;
-
 
 namespace FFXIV_Disable_Caps
 {
     public partial class Options : Form
     {
+
+        #region Global Declarations
 
         private const int WH_KEYBOARD_LL = 13;
         private const int VK_CONTROL = 17;
@@ -42,27 +37,57 @@ namespace FFXIV_Disable_Caps
         private IntPtr ptrHook;
         private LowLevelKeyboardProc objKeyboardProcess;
 
+        #endregion
+
+        #region Form Functions
         public Options()
         {
             InitializeComponent();
 
             using (ProcessModule mainModule = Process.GetCurrentProcess().MainModule)
             {
-                objKeyboardProcess = new LowLevelKeyboardProc(captureKey);
+                objKeyboardProcess = new LowLevelKeyboardProc(CaptureKeyPress);
                 ptrHook = SetWindowsHookEx(13, objKeyboardProcess, GetModuleHandle(mainModule.ModuleName), 0U);
             }
         }
 
-        public IntPtr captureKey(int nCode, IntPtr wp, IntPtr lp)
+        private void Options_Load(object sender, EventArgs e)
+        {
+            ReadStartup();
+        }
+
+        private void Options_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnhookWindowsHookEx(ptrHook);
+        }
+
+        private void Options_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.TrayIcon.Visible = true;
+                this.Hide();
+            }
+            else
+                this.TrayIcon.Visible = false;
+        }
+
+        private void TrayIcon_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+        #endregion
+
+        #region KeyPress Functions
+        public IntPtr CaptureKeyPress(int nCode, IntPtr wp, IntPtr lp)
         {
             if (Gamerunning && nCode >= 0)
             {
                 KBDLLHOOKSTRUCT objKeyInfo = (KBDLLHOOKSTRUCT)Marshal.PtrToStructure(lp, typeof(KBDLLHOOKSTRUCT));
 
                 if (objKeyInfo.key == Keys.Capital)
-                {
                     return (IntPtr)1;
-                }
             }
             return CallNextHookEx(ptrHook, nCode, wp, lp);
         }
@@ -75,17 +100,18 @@ namespace FFXIV_Disable_Caps
                 Gamerunning = true;
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void timert_check_process(object sender, EventArgs e)
         {
             CheckProcess();
         }
 
+        #endregion
 
+        #region Registry Functions
         private void SetStartup()
         {
             string AppName = "FFXIV Caps Lock Disabler";
-            RegistryKey rk = Registry.CurrentUser.OpenSubKey
-                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
 
             if (chkAutoStart.Checked)
                 rk.SetValue(AppName, Application.ExecutablePath);
@@ -93,30 +119,18 @@ namespace FFXIV_Disable_Caps
                 rk.DeleteValue(AppName, false);
 
         }
-
         private void ReadStartup()
         {
             string AppName = "FFXIV Caps Lock Disabler";
-            RegistryKey rk = Registry.CurrentUser.OpenSubKey
-                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
 
-            var sStartup = rk.GetValue(AppName);
-            if (sStartup == null)
-                chkAutoStart.Checked = false;
-            else
-                chkAutoStart.Checked = true;
-
+            chkAutoStart.Checked = (rk.GetValue(AppName) == null) ? false : true;
         }
-
-
-        private void Options_Load(object sender, EventArgs e)
-        {
-            ReadStartup();
-        }
-
         private void chkAutoStart_Click(object sender, EventArgs e)
         {
             SetStartup();
         }
+
+        #endregion
     }
 }
